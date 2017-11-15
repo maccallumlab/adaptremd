44eb2ff7ff5eae8d988d62c65132213eb22744c4
Justin MacCallum <justin.maccallum@ucalgary.ca>
2017-11-14 18:36:51 -0700

    Switched harmonic_plot to use log scale

--- harmonic_plot.py
+++ harmonic_plot.py
@@ -1,13 +1,14 @@
 import math
 from matplotlib import pyplot as pp
 import numpy as np
+import eig
 
 
 R = 8.314e-3
 TMIN = 10.
 TMAX = 10000.
-N = 100
-NSAMP = 100000
+N = 20
+NSAMP = 10000
 
 d = (TMAX - TMIN) / float(N)
 extent = (TMIN-0.5*d, TMAX+0.5*d, TMIN-0.5*d, TMAX+0.5*d)
@@ -32,10 +33,10 @@ def compute_acc(T1, T2):
 
 def compute_rt(a1, a2, a3):
     P = np.array([
-        [1.0 - a1,                          a1,              0.0,                 0.0],
-        [0.5 * a1, (1.0 - 0.5 * a1 - 0.5 * a2),              0.5 * a2,            0.0],
-        [     0.0,                    0.5 * a2, (1.0 - 0.5 * a2 - 0.5 * a3), 0.5 * a3],
-        [     0.0,                         0.0,                          a3, 1.0 - a3]
+        [1.0 - 0.5 * a1,              0.5 * a1,                   0.0,            0.0],
+        [      0.5 * a1, 1.0 - 0.5 * (a1 + a2),              0.5 * a2,            0.0],
+        [           0.0,              0.5 * a2, 1.0 - 0.5 * (a2 + a3),       0.5 * a3],
+        [           0.0,                   0.0,              0.5 * a3, 1.0 - 0.5 * a3]
     ])
     e1 = np.array([1.0, 0.0, 0.0])
     e1.shape = 1, 3
@@ -43,8 +44,10 @@ def compute_rt(a1, a2, a3):
     e.shape = 3, 1
 
     Q = np.eye(3, 3) - P[:-1, :-1]
-    x = np.linalg.lstsq(Q, e)[0]
-    rt = np.dot(e1, x)
+    x = np.linalg.inv(Q)
+    rt = np.dot(e1, np.dot(x, e))
+    rt = eig.fmpt(P)[0, -1]
+    print rt
     return rt
 
 
@@ -55,23 +58,45 @@ def make_plot(temps, data):
     pp.imshow(data.T, interpolation='nearest', origin='lower',
               extent=extent)
     pp.colorbar()
-    # pp.quiver(Y, X, xgrad, ygrad, pivot='mid')
+
+    # draw lines for expected results
+    t2 = TMIN * (TMAX / TMIN)**(1.0 / 3.0)
+    t3 = TMIN * (TMAX / TMIN)**(2.0 / 3.0)
+    print t2, t3
+    pp.axhline(t3, color='white', linewidth=3)
+    pp.axvline(t2, color='white', linewidth=3)
+    pp.axhline(t3, color='black', linewidth=1)
+    pp.axvline(t2, color='black', linewidth=1)
     speed = (np.sqrt(xgrad**2 + ygrad**2))
     lw = 2 * np.sqrt(speed / speed.max())
     pp.streamplot(temps, temps, xgrad.T, ygrad.T, color='black', density=1.0,
                   linewidth=lw)
 
 
-if __name__ == '__main__':
-    res1 = np.zeros((N, N))
-    res2 = np.zeros((N, N))
-    res3 = np.zeros((N, N))
-    res4 = np.zeros((N, N))
-    res5 = np.zeros((N, N))
-    res6 = np.zeros((N, N))
-    res7 = np.zeros((N, N))
+def make_plot2(temps, data):
+    X, Y = np.meshgrid(temps, temps)
+    pp.figure()
+    pp.pcolor(X, Y, data.T)
+    pp.colorbar()
+    pp.xscale('log')
+    pp.yscale('log')
 
-    temps = np.linspace(TMIN, TMAX, N)
+    # draw lines for expected results
+    t2 = TMIN * (TMAX / TMIN)**(1.0 / 3.0)
+    t3 = TMIN * (TMAX / TMIN)**(2.0 / 3.0)
+    pp.axhline(t3, color='white', linewidth=3)
+    pp.axvline(t2, color='white', linewidth=3)
+    pp.axhline(t3, color='black', linewidth=1)
+    pp.axvline(t2, color='black', linewidth=1)
+
+
+if __name__ == '__main__':
+    round_trip_results = np.zeros((N, N))
+    uniform_acc_results = np.zeros((N, N))
+    total_acc_results = np.zeros((N, N))
+    log_total_acc_results = np.zeros((N, N))
+
+    temps = np.logspace(start=1, stop=4, num=N, base=10)
     for i, T2 in enumerate(temps):
         for j, T3 in enumerate(temps):
             a1 = compute_acc(TMIN, T2)
@@ -81,24 +106,16 @@ if __name__ == '__main__':
             ln1 = math.log(a1)
             ln2 = math.log(a2)
             ln3 = math.log(a3)
-            mean = np.mean([ln1, ln2, ln3])
-            # mean = math.log(0.90)
+            mean = np.mean([a1, a2, a3])
 
-            res1[i, j] = ln1 + ln2 + ln3
-            res2[i, j] = -(ln1 - mean)**2 - (ln2 - mean)**2 - (ln3 - mean)**2
-            res3[i, j] = a1 * a2 * a3
-            res4[i, j] = a1
-            res5[i, j] = a2
-            res6[i, j] = a3
-            res7[i, j] = compute_rt(a1, a2, a3)
+            round_trip_results[i, j] = compute_rt(a1, a2, a3)
+            uniform_acc_results[i, j] = -(a1 - mean)**2 - (a2 - mean)**2 - (a3 - mean)**2
+            total_acc_results[i, j] = a1 * a2 * a3
+            log_total_acc_results[i, j] = ln1 + ln2 + ln3
 
 
-    make_plot(temps, res1)
-    # make_plot(temps, res2)
-    make_plot(temps, res3)
-    # make_plot(temps, res1 + res2)
-    make_plot(temps, res4)
-    make_plot(temps, res5)
-    make_plot(temps, res6)
-    make_plot(temps, -np.log(res7))
+    make_plot2(temps, 1.0 / round_trip_results)
+    make_plot2(temps, uniform_acc_results)
+    make_plot2(temps, total_acc_results)
+    make_plot2(temps, log_total_acc_results)
     pp.show()
